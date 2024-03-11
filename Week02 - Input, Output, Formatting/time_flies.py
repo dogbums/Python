88c523727f4dfbcc319b3210eef8317db0d05a9f
dogbums <bigbadwilf@gmail.com>
2024-03-11 12:00:14 +0000

    Completed code

--- Week02 - Input, Output, Formatting/time_flies.py
+++ Week02 - Input, Output, Formatting/time_flies.py
@@ -2,6 +2,6 @@
 
 age = int(input("Please enter your age:"))
 
-print ("Current age is:" + str(age))
+print ("Current age is: " + str(age))
 print ("Age before last birthday was: " + str(age - 1))
 print ("Age after next birthday: " + str(age + 1))
