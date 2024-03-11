b42f0820397414774b490795763ef6a7f9230560
Callum Yates <47091219+CptFlashbang@users.noreply.github.com>
2024-03-11 20:55:12 +0000

    added comments for clarity and labelling

--- Week01 - Intro to Python/breakeven.py
+++ Week01 - Intro to Python/breakeven.py
@@ -14,12 +14,14 @@ breakeven = (fixedcosts / profitperitem)
 # print (("Profit per item: ") , profitperitem)
 # print (("Break even:") , breakeven , ("items"))
 
+#String concatanation
 # print (("Fixed costs: ") , fixedcosts)
 # print (("Sale price for each item: ") + str(saleprice))
 # print (("Cost to produce each item: ") + str(itemcost))
 # print (("Profit per item: ") + str(profitperitem))
 # print (("Break even: ") + str(breakeven) , ("items"))
 
+#F-string method
 print(f"Fixed costs: {fixedcosts}")
 print(f"Sale price for each item: {saleprice}")
 print(f"Cost to produce each item: {itemcost}")
