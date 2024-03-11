d67f4c5802086a19656716a476809ab5a68d6c41
dogbums <bigbadwilf@gmail.com>
2024-03-11 15:45:19 +0000

    Added print commands

--- Week02 - Input, Output, Formatting/cuboid.py
+++ Week02 - Input, Output, Formatting/cuboid.py
@@ -7,7 +7,8 @@ length = int(input ("Enter length: "))
 volume = (height * width * length)
 surface_area = ((height * width * 2) + (width * length * 2) + height * length * 2)
 
-
+print (("Volume: ") + volume)
+print (("Surface area: " + surface_area))
 
 
 
