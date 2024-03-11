e1aa37dd911c7cd8eb349f8239fd65cf7c4ecd7f
dogbums <bigbadwilf@gmail.com>
2024-03-11 13:17:05 +0000

    Ran unsuccessfully, adding brackets to print lines

--- Week02 - Input, Output, Formatting/address.py
+++ Week02 - Input, Output, Formatting/address.py
@@ -1,1 +1,8 @@
-surname = str()
+surname = str(input("Please enter your surname: "))
+housenumber = int(input("Please enter your house number: "))
+roadname = str(input("Please enter your road name: "))
+town = str(input("Please enter your town: "))
+
+print (surname) 
+print (housenumber) print (roadname)
+print (town)
