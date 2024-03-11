2be8ae5fde57ed76c2cfc7f71bb914bb1a8f4614
dogbums <bigbadwilf@gmail.com>
2024-03-11 15:43:17 +0000

    Entered the mathmatical sums for volume and surface area

--- Week02 - Input, Output, Formatting/cuboid.py
+++ Week02 - Input, Output, Formatting/cuboid.py
@@ -1,8 +1,14 @@
-# Program for measuring volume
+# Program for measuring volume and surface area of a cuboid
 
 height = int(input("Enter height: "))
-width = int(input ("Enter height: "))
+width = int(input ("Enter width: "))
 length = int(input ("Enter length: "))
+
+volume = (height * width * length)
+surface_area = ((height * width * 2) + (width * length * 2) + height * length * 2)
+
+
+
 
 
 
