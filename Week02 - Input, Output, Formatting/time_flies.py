c814089c2ca30a1210cb497e03c6dbc36d977345
dogbums <bigbadwilf@gmail.com>
2024-03-11 11:59:06 +0000

    Ran program successfully, added spacing to printed text descriptor to improve visuals

--- Week02 - Input, Output, Formatting/time_flies.py
+++ Week02 - Input, Output, Formatting/time_flies.py
@@ -1,1 +1,7 @@
-age = int(input("Please enter your age"))
+#program to output someone's current age, age before last birthday and age after their next birthday
+
+age = int(input("Please enter your age:"))
+
+print ("Current age is:" + str(age))
+print ("Age before last birthday was: " + str(age - 1))
+print ("Age after next birthday: " + str(age + 1))
