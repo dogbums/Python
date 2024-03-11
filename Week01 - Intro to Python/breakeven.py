f7bd3a777306a9b51ec4b4207b44e1fd1e42069c
dogbums <bigbadwilf@gmail.com>
2024-03-11 11:24:42 +0000

    Changed values to integers

---
+++ Week01 - Intro to Python/breakeven.py
@@ -0,0 +1,13 @@
+# program to figure out how many products must be sold for a company to break even
+
+fixedcosts = int(50000)
+saleprice = int(40)
+itemcost = int(20)
+
+profitperitem = (saleprice - itemcost)
+
+breakeven = (profitperitem / fixedcosts)
+
+
+print (profitperitem)
+print (breakeven)
