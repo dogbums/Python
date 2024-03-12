9ae7f147910ba845d0fb60326366a12649d790b6
dogbums <bigbadwilf@gmail.com>
2024-03-12 12:41:42 +0000

    Deleted unnecessary comments that were blanked out incorrect code

--- Week01 - Intro to Python/sweet_tooth.py
+++ Week01 - Intro to Python/sweet_tooth.py
@@ -10,13 +10,6 @@ sweetsforstudents = (totalnumberofsweets - sweetsforteacher)
 
 sweetsperstudent = (sweetsforstudents / numberofstudents)
 
-#totalsweetsforstudents = (totalnumberofsweets % numberofstudents)
-
-#sweetsperstudent = (totalsweetsforstudents / 14)
-
-#sweetsforteacher = (totalnumberofsweets - sweetsperstudent * 14)
-
-#print(f"Number of sweets each: {sweetsperstudent}")
 
 print(f"Sweets per student: {sweetsperstudent}")
 
