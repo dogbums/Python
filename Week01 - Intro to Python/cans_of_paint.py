639c2d0d9e3a15dfc5c159a70d5ec5a8ff500991
dogbums <bigbadwilf@gmail.com>
2024-03-12 15:45:21 +0000

    Successfully ran program using math.ceil to calculate the number of cans required

--- Week01 - Intro to Python/cans_of_paint.py
+++ Week01 - Intro to Python/cans_of_paint.py
@@ -1,7 +1,15 @@
 #program to breakdown how much paint is required to paint some walls
 
-totalareapainted = (30 * 40)
-
 import math
 
+areapercan = (5.1)
+
+totalareapainted = ((40 * 3.4 * 2) + (30 * 3.4 *2))
+
+numberofcans = math.ceil(totalareapainted / areapercan)
+
+boxarea = (0.60 * 0.35)
+
 print (totalareapainted)
+print (boxarea)
+print (numberofcans)
