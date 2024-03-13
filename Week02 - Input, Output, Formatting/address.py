62cee49c3a23c76335fd65d759d7d39b578a1390
dogbums <bigbadwilf@gmail.com>
2024-03-13 14:06:54 +0000

    Changed print commands into F strings for simplicity

--- Week02 - Input, Output, Formatting/address.py
+++ Week02 - Input, Output, Formatting/address.py
@@ -5,6 +5,10 @@ housenumber = int(input("Please enter your house number: "))
 roadname = str(input("Please enter your road name: "))
 town = str(input("Please enter your town: "))
 
-print (surname) 
-print (housenumber , end="") , print (", " , end="") , print (roadname)
-print (town)
+# print ("Mr and Mrs Windsor") 
+# print ("1, The Mall")
+# print ("London")
+
+print (f"Mr and Mrs {surname}") 
+print (f"{housenumber}, {roadname}")
+print (f"{town}")
