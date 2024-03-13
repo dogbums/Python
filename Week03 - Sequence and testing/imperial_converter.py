60e301f1b6f63e2c3e43b3f1456ff9747f2732d5
dogbums <bigbadwilf@gmail.com>
2024-03-13 20:31:22 +0000

    Attempting to clean up output

--- Week03 - Sequence and testing/imperial_converter.py
+++ Week03 - Sequence and testing/imperial_converter.py
@@ -2,18 +2,18 @@ import math
 
 print("Please input height in feet and inches: ")
 
-feet = input("Feet: ")
-inches = input("Inches: ")
+feet = float(input("Feet: "))
+inches = float(input("Inches: "))
 
 # 1 foot = 0.305 metres
 # 1 inch = 0.025 metres
 
-kilometres = (feet*0.305*1000)
-metres
-centimetres
-millimetres
+kilometres = (feet*0.305/1000)
+# metres
+# centimetres
+# millimetres
 
-print(kilometres)
+print(f"Kilometres: "{kilometres:.6f}")
 
 
 
