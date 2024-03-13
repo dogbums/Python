231cca126009b8ce031a48a72f893591c42c891b
dogbums <bigbadwilf@gmail.com>
2024-03-13 20:21:29 +0000

    Ran code, inputs work but does not excute properly

---
+++ Week03 - Sequence and testing/imperial_converter.py
@@ -0,0 +1,19 @@
+import math
+
+print("Please input height in feet and inches: ")
+
+feet = input("Feet: ")
+inches = input("Inches: ")
+
+# 1 foot = 0.305 metres
+# 1 inch = 0.025 metres
+
+kilometres = (feet*0.305*1000)
+metres
+centimetres
+millimetres
+
+print(kilometres)
+
+
+
