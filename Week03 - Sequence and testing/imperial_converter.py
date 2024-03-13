eda35a13bdd2bc5f8e7278b413ff06ce81a19f7e
dogbums <bigbadwilf@gmail.com>
2024-03-13 20:50:14 +0000

    Deleted code that made no sense

--- Week03 - Sequence and testing/imperial_converter.py
+++ Week03 - Sequence and testing/imperial_converter.py
@@ -2,18 +2,20 @@ import math
 
 print("Please input height in feet and inches: ")
 
-feet = float(input("Feet: "))
-inches = float(input("Inches: "))
+feet = int(input("Feet: "))
+inches = int(input("Inches: "))
 
 # 1 foot = 0.305 metres
 # 1 inch = 0.025 metres
 
-kilometres = (feet*0.305/1000)
+
 # metres
 # centimetres
 # millimetres
 
 print(f"Kilometres: {kilometres:.6f}")
+
+print(f"Metres: {heightinmetres}")
 
 
 
