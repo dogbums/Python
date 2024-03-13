9ce0b1cf53e9052c527c5ab4bfdd38f3d5d6f836
dogbums <bigbadwilf@gmail.com>
2024-03-13 17:28:39 +0000

    Commented out calculations

--- Week02 - Input, Output, Formatting/distance_calculator.py
+++ Week02 - Input, Output, Formatting/distance_calculator.py
@@ -2,9 +2,11 @@ initial_velocity = input("Enter initial velocity: ")
 time = input("Enter time: ")
 acceleration = input("Enter acceleration: ")
 
-UT = (initial_velocity * time)
-T2 = (time * time)
-AT2 = (acceleration * T2)
-S = (UT + (0.5 * AT2))
+# UT = (initial_velocity * time)
+# T2 = (time * time)
+# AT2 = (acceleration * T2)
+# S = (UT + (0.5 * AT2))
+
+
 
 print(S)
