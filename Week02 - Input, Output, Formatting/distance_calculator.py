3f4e74c1c758117f4048f24be38d8014aafc483f
dogbums <bigbadwilf@gmail.com>
2024-03-13 17:43:56 +0000

    Entered formula on one line

--- Week02 - Input, Output, Formatting/distance_calculator.py
+++ Week02 - Input, Output, Formatting/distance_calculator.py
@@ -1,12 +1,13 @@
-initial_velocity = input("Enter initial velocity: ")
-time = input("Enter time: ")
-acceleration = input("Enter acceleration: ")
+u = input("Enter initial velocity: ")
+t = input("Enter time: ")
+a = input("Enter acceleration: ")
 
 # UT = (initial_velocity * time)
 # T2 = (time * time)
 # AT2 = (acceleration * T2)
 # S = (UT + (0.5 * AT2))
 
+S = (u*t) + (0.5*a*(t*t))
 
 
 print(S)
