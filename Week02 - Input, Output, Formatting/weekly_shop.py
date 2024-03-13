ec387757cec5bfb04704466b5fa0c64a417bf3be
dogbums <bigbadwilf@gmail.com>
2024-03-13 14:40:08 +0000

    Created new file

---
+++ Week02 - Input, Output, Formatting/weekly_shop.py
@@ -0,0 +1,13 @@
+peaches = input()
+beans = input()
+chickenpieces = input()
+socks = input()
+bottlesofwater = input()
+
+input (f"Enter number of peaches: {peaches}")
+input (f"Enter number of cans of beans: {beans}")
+input (f"Enter number of packs of chicken pieces: {chickenpieces}")
+input (f"Enter number of pairs of socks: {socks}")
+input (f"Enter number of bottles of water: {bottlesofwater} ")
+
+print (f"Peaches {peaches}")
