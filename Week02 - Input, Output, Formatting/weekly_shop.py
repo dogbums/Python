a9a46ce059ad984e8356c3fcc46d421940f69394
dogbums <bigbadwilf@gmail.com>
2024-03-13 15:51:15 +0000

    Deleted code that was trying to input variables without first stating them

--- Week02 - Input, Output, Formatting/weekly_shop.py
+++ Week02 - Input, Output, Formatting/weekly_shop.py
@@ -1,8 +1,4 @@
-# input (f"Enter number of peaches: {peaches}")
-# input (f"Enter number of cans of beans: {beans}")
-# input (f"Enter number of packs of chicken pieces: {chickenpieces}")
-# input (f"Enter number of pairs of socks: {socks}")
-# input (f"Enter number of bottles of water: {bottlesofwater} ")
+
 
 # print (f"Peaches {peaches}")
 
