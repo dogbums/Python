b4ab2e9331c5a4d7cf8c1db898a0255dc9fe027f
dogbums <bigbadwilf@gmail.com>
2024-03-13 16:17:56 +0000

    Made experimental code into comments and uncommented previous working code

--- Week02 - Input, Output, Formatting/weekly_shop.py
+++ Week02 - Input, Output, Formatting/weekly_shop.py
@@ -1,32 +1,32 @@
 # A program designed to input and calculate the price of a weekly shop and display the total number of items as well as the 
 # number of individual item and cost
 
-# peaches = input("Enter number of peaches: ")
-# beans = input("Enter number of cans of beans: ")
-# chickenpieces = input("Enter number of packets of chicken pieces: ")
-# socks = input("Enter number of pairs of socks: ")
-# bottlesofwater = input("Enter number of bottles of water: ")
+peaches = input("Enter number of peaches: ")
+beans = input("Enter number of cans of beans: ")
+chickenpieces = input("Enter number of packets of chicken pieces: ")
+socks = input("Enter number of pairs of socks: ")
+bottlesofwater = input("Enter number of bottles of water: ")
 
 #REMOVE THIS AFTER DEVELOPMENT IS COMPLETE
-peaches = "6"
-beans = "5"
-chickenpieces = "6"
-socks = "3"
-bottlesofwater = "2"
+# peaches = "6"
+# beans = "5"
+# chickenpieces = "6"
+# socks = "3"
+# bottlesofwater = "2"
 
-# priceofpeaches = input("Enter the price of a peach: ")
-# priceofbeans = input("Enter the price of a can of beans: ")
-# priceofchickenpieces = input ("Enter the price of a packet of chicken pieces: ")
-# priceofsocks = input("Enter the price of a pair of socks: ")
-# priceofbottlesofwater = input("Enter the price of a bottle of water: ")
+priceofpeaches = input("Enter the price of a peach: ")
+priceofbeans = input("Enter the price of a can of beans: ")
+priceofchickenpieces = input ("Enter the price of a packet of chicken pieces: ")
+priceofsocks = input("Enter the price of a pair of socks: ")
+priceofbottlesofwater = input("Enter the price of a bottle of water: ")
 
 #REMOVE THIS AFTER DEVELOPMENT IS COMPLETE
-peaches = "6"
-priceofpeaches = "0.60"
-priceofbeans = "1"
-priceofchickenpieces = "2.50"
-priceofsocks = "4.00"
-priceofbottlesofwater = "1.50"
+# peaches = "6"
+# priceofpeaches = "0.60"
+# priceofbeans = "1"
+# priceofchickenpieces = "2.50"
+# priceofsocks = "4.00"
+# priceofbottlesofwater = "1.50"
 
 totalnumberofitems = (int(peaches) + int(beans) + int(chickenpieces) + int(socks) + int(bottlesofwater))
 
