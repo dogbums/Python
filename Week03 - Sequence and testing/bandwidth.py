869638ca80355dad4fc9b6a01a71edbd6d012c3f
dogbums <bigbadwilf@gmail.com>
2024-03-14 13:00:59 +0000

    Gave more values, including calculations, conversions and printed

--- Week03 - Sequence and testing/bandwidth.py
+++ Week03 - Sequence and testing/bandwidth.py
@@ -3,4 +3,18 @@ BPS = 1
 
 MBPS = BPS/1000000
 
-print(BPS)
+MAX = 1000/MBPS
+
+APPA = 200000*BPS
+
+APPB = 100000*BPS
+
+CURRENT = (APPA + APPB)
+
+NEWAPP = 350000*BPS
+
+print(f"Maximum network capacity: {MAX}")
+
+print(f"Current network usage: {APPA + APPB}")
+
+print(f"Current availability: {MAX%CURRENT}")
