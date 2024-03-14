b2b87c20227e8b8450b785ddb22f577c44aaca91
dogbums <bigbadwilf@gmail.com>
2024-03-14 16:06:35 +0000

    Added £ symbols to outputs for lines 13, 15, 17

--- Week03 - Sequence and testing/telephone_bill.py
+++ Week03 - Sequence and testing/telephone_bill.py
@@ -10,8 +10,8 @@ VAT = (callcharge)*0.2
 
 print(f"Number of minutes used: {minutes}")
 
-print(f"Basic call charge: {callcharge:.2f}")
+print(f"Basic call charge: £{callcharge:.2f}")
 
-print(f"VAT due: {VAT:.2f}")
+print(f"VAT due: £{VAT:.2f}")
 
-print(f"Total bill: {totalbill:.2f}")
+print(f"Total bill: £{totalbill:.2f}")
