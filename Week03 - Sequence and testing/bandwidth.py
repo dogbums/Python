c3c998660db0f7ef48eea688461ecd639292e1db
dogbums <bigbadwilf@gmail.com>
2024-03-14 13:26:15 +0000

    Added new function to line 18, then printed (correct result)

--- Week03 - Sequence and testing/bandwidth.py
+++ Week03 - Sequence and testing/bandwidth.py
@@ -1,20 +1,28 @@
 
 BPS = 1
 
-MBPS = BPS/1000000
+MBPS = BPS*1000000
 
-MAX = 1000/MBPS
+MAX = 1000*MBPS
 
-APPA = 200000*BPS
+APPA = float (200000*BPS)
 
-APPB = 100000*BPS
+APPB = float (100000*BPS)
 
-CURRENT = (APPA + APPB)
+CURRENT = float (APPA + APPB)*200
 
-NEWAPP = 350000*BPS
+NEWAPP = (350000*BPS)*200
+
+LEFTOVER = (MAX-CURRENT)
+
+NEWAVAIL = (LEFTOVER -NEWAPP)/1000000
 
 print(f"Maximum network capacity: {MAX}")
 
-print(f"Current network usage: {APPA + APPB}")
+print(f"Current network usage: {CURRENT}")
 
-print(f"Current availability: {MAX%CURRENT}")
+print(f"Current availability: {LEFTOVER}")
+
+print(f"New application requirement: {NEWAPP}")
+
+print(f"Bandwidth available after new app is installed {NEWAVAIL}")
