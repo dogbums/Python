eea89f9235544655aa22fff2a529b044b8dac302
dogbums <bigbadwilf@gmail.com>
2024-03-14 15:56:11 +0000

    Added more calculations and printed correct results

---
+++ Week03 - Sequence and testing/telephone_bill.py
@@ -0,0 +1,17 @@
+# A program that calculates call charges
+
+minutes =float(input("Please enter number of minutes: "))
+
+callcharge = (minutes*0.15)
+
+totalbill = (callcharge)*1.2
+
+VAT = (totalbill)*0.2
+
+print(minutes)
+
+print(callcharge)
+
+print(VAT)
+
+print(totalbill)
