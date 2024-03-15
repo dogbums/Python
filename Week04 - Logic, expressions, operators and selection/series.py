d6b823e34bf9a4065f4451d07fe9cc58aba33072
dogbums <bigbadwilf@gmail.com>
2024-03-15 21:41:12 +0000

    Set out logic for separating negative and positive numbers

--- Week04 - Logic, expressions, operators and selection/series.py
+++ Week04 - Logic, expressions, operators and selection/series.py
@@ -1,12 +1,44 @@
 print("Please input 5 whole numbers (can be either positive or negative, but not zero): ")
 
 num1 = int(input("Please enter the first whole number: "))
-# num2 = input("Please enter the second whole number: ")
-# num3 = input("Please enter the third whole number: ")
-# num4 = input("Please enter the fourth whole number: ")
-# num5 = input("Please enter the fifth whole number: ")
+num2 = input("Please enter the second whole number: ")
+num3 = input("Please enter the third whole number: ")
+num4 = input("Please enter the fourth whole number: ")
+num5 = input("Please enter the fifth whole number: ")
+
+num1neg = int
+num1pos = int
+num2neg = int
+num2pos = int
+num3neg = int
+num3pos = int
+num4neg = int
+num4pos = int
+num5neg = int
+num5pos = int
 
 if (num1 > 0):
-    print("Positive")
+    num1 = num1pos
 else:
-    print("Negative")
+    num1 = num1neg
+
+if (num2 > 0):
+    num2 = num2pos
+else:
+    num2 = num2neg
+
+if (num3 > 0):
+    num3 = num3pos
+else:
+    num3 = num3neg
+
+if (num4 > 0):
+    num4 = num4pos
+else:
+    num4 = num4neg
+
+if (num5 > 0):
+    num5 = num5pos
+else:
+    num5 = num5neg
+
