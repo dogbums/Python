39921dbf554b2489db7c7d971864a7a4dca5c6cb
dogbums <bigbadwilf@gmail.com>
2024-03-15 21:11:30 +0000

    Used > symbol to determine if an integer is positive or negative

--- Week04 - Logic, expressions, operators and selection/series.py
+++ Week04 - Logic, expressions, operators and selection/series.py
@@ -1,7 +1,12 @@
 print("Please input 5 whole numbers (can be either positive or negative, but not zero): ")
 
-num1 = input("Please enter the first whole number: ")
-num2 = input("Please enter the second whole number: ")
-num3 = input("Please enter the third whole number: ")
-num4 = input("Please enter the fourth whole number: ")
-num5 = input("Please enter the fifth whole number: ")
+num1 = int(input("Please enter the first whole number: "))
+# num2 = input("Please enter the second whole number: ")
+# num3 = input("Please enter the third whole number: ")
+# num4 = input("Please enter the fourth whole number: ")
+# num5 = input("Please enter the fifth whole number: ")
+
+if (num1 > 0):
+    print("Positive")
+else:
+    print("Negative")
