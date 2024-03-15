3e0c83fa8ce6cfdb92617b494ddc991eacf40304
dogbums <bigbadwilf@gmail.com>
2024-03-15 15:44:23 +0000

    Added method to output correct result based on input

--- Week04 - Logic, expressions, operators and selection/magpie.py
+++ Week04 - Logic, expressions, operators and selection/magpie.py
@@ -1,1 +1,25 @@
-number = input("Please a whole number between 1 and 7:")
+number = int(input("Please a whole number between 1 and 7: "))
+
+if number == 1:
+    print("One for sorrow")
+
+if number == 2:
+    print("Two for joy")
+
+if number == 3:
+    print("Three for a girl")
+
+if number == 4:
+    print("Four for a boy")
+
+if number == 5:
+    print("Five for silver")
+
+if number == 6:
+    print("Six for gold")
+
+if number == 7:
+    print("Seven for a secret")
+
+else:
+    print("Not a permitted number")
