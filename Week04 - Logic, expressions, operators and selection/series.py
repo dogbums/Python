6a83f8ac9f9fef3a7ba7cda36d3c5b316a3bfb71
dogbums <bigbadwilf@gmail.com>
2024-03-15 21:00:44 +0000

    Started writing new program

---
+++ Week04 - Logic, expressions, operators and selection/series.py
@@ -0,0 +1,7 @@
+print("Please input 5 whole numbers (can be either positive or negative, but not zero): ")
+
+num1 = input("Please enter the first whole number: ")
+num2 = input("Please enter the second whole number: ")
+num3 = input("Please enter the third whole number: ")
+num4 = input("Please enter the fourth whole number: ")
+num5 = input("Please enter the fifth whole number: ")
