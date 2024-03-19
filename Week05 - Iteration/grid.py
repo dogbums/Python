49c791607b895cb8b57ddc36c52382acac317d6e
dogbums <bigbadwilf@gmail.com>
2024-03-19 13:19:49 +0000

    Opened new file and tested maths method

---
+++ Week05 - Iteration/grid.py
@@ -0,0 +1,12 @@
+rows = int(input("Input number of rows: "))
+columns = int(input("Input number of columns: "))
+
+columns = ("*" * columns)
+rows = ("*" * rows)
+
+print(columns)
+
+print(rows)
+
+# for (rows):
+#     print (f"{columns} \n")
