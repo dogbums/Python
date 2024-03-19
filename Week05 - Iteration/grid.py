d18ed3ac26aba880cfc57aa5d2ec57e8167d6b00
dogbums <bigbadwilf@gmail.com>
2024-03-19 14:23:57 +0000

    Added comment to grid.py and created new file

--- Week05 - Iteration/grid.py
+++ Week05 - Iteration/grid.py
@@ -1,3 +1,5 @@
+# A program that inputs the number of rows and columns then outputs a grid of asterisks in the correct arrangement
+
 rows = int(input("Input number of rows: "))
 columns = int(input("Input number of columns: "))
 
