c9a1695055edf01eaa954a0a9caddec87ad7b435
dogbums <bigbadwilf@gmail.com>
2024-03-25 18:49:01 +0000

    Added range

---
+++ Week05 - Iteration/grid2.py
@@ -0,0 +1,18 @@
+# A program that inputs the number of rows and columns then outputs a grid of asterisks in the correct arrangement
+
+# rows = int(input("Input number of rows: "))
+# columns = int(input("Input number of columns: "))
+
+# columns = (f"* " * columns)
+
+# print ((f"{columns}\n") * rows)
+
+rows = int(input("Input number of rows: "))
+columns = int(input("Input number of columns: "))
+
+for x in range(1, columns):
+    print(f"*" * rows)
+
+
+
+
