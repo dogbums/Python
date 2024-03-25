e4efa03fa5ed42109fafe33d0d9a33a069530b36
dogbums <bigbadwilf@gmail.com>
2024-03-25 22:23:54 +0000

    Added append method to lines 22, 24

--- Week05 - Iteration/average_pos_neg.py
+++ Week05 - Iteration/average_pos_neg.py
@@ -11,21 +11,22 @@ num8 = int(input("Please enter the eight whole number: "))
 num9 = int(input("Please enter the ninth whole number: "))
 num10 = int(input("Please enter the tenth whole number: "))
 
-number_list = num1, num2, num3, num4, num5, num6, num7, num8, num9, num10
+number_list = [num1, num2, num3, num4, num5, num6, num7, num8, num9, num10]
 
-positive_list = 0
+positive_list = []
 
-negative_list = 0
+negative_list = []
 
 for r in number_list:
     if r > 0:
-        positive_list += r
+        positive_list.append(r)
     else:
-        negative_list += r
+        negative_list.append(r)
 
-average_of_pos = positive_list /5
 
-average_of_neg = negative_list /5
+average_of_pos = sum(positive_list) / len(positive_list)
+
+average_of_neg = sum(negative_list) / len(negative_list)
 
 print(f"Sum of positives: {positive_list}")
 print(f"Average of positives: {average_of_pos}")
