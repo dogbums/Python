4c4cda96118c85899ef3759236861fdc57cf8ae0
dogbums <bigbadwilf@gmail.com>
2024-03-27 14:04:40 +0000

    Started new program, tested if statement

---
+++ Week05 - Iteration/reversal.py
@@ -0,0 +1,4 @@
+number = int(input("Please enter a number between 1 and 11 digits long, alternatively enter a negative number to quit: "))
+
+if number > 99999999999:
+    print("This number is invalid")
