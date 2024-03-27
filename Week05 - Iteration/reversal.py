fb3a4f70b8ad93ca4d6b5056bf77d72187432c42
dogbums <bigbadwilf@gmail.com>
2024-03-27 16:19:56 +0000

    Added inputs and while statement

--- Week05 - Iteration/reversal.py
+++ Week05 - Iteration/reversal.py
@@ -1,22 +1,38 @@
-number = input("Please enter a number between 1 and 11 digits long, or enter a negative number to quit: ")
+# number = input("Please enter a number between 1 and 11 digits long, or enter a negative number to quit: ")
 
-dcount = 0
+# dcount = 0
 
-for d in number:
+# for d in number:
 
-    if d == "-":
-         break
+#     if d == "-":
+#          break
 
-    else: dcount += 1
+#     else: dcount += 1
     
 
-while dcount > 11:
+# while dcount > 11:
 
 
-        print("The number is invalid")
-        break
+#         print("The number is invalid")
+#         break
     
 
-if dcount <= 11:
-        print("The number reversed is: ")
-        print(number[::-1])
+# if dcount <= 11:
+#         print("The number reversed is: ")
+#         print(number[::-1])
+
+number = int(input("Please enter a number between 2 and 11 digits long, or enter a negative number to quit: "))
+
+rev = 0
+
+# while number < 0:
+#       print("Invalid number")
+#       number = int(input("Please enter a number between 2 and 11 digits long, or enter a negative number to quit: "))
+
+while number > 9:
+     rev = rev * 10 + (number % 10)
+     number = number // 10
+print(f"Number reversed: {rev}")
+
+if number < 10:
+       print("Invalid number")
